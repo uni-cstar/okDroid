fe4ec088713a8f42cc735167f0148d8bb35f8ea4
unics <super_luo@163.com>
2025-03-05 00:12:27 +0800

    feat: 完成隐私协议；部分焦点逻辑优化

--- okDroid/src/main/java/unics/okdroid/tools/fragment/FullScreenDialogFragment.kt
+++ okDroid/src/main/java/unics/okdroid/tools/fragment/FullScreenDialogFragment.kt
@@ -5,6 +5,7 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.DialogFragment
+import unics.okdroid.R
 
 /**
  * Created by Lucio on 2021/6/4.
@@ -17,11 +18,25 @@ abstract class FullScreenDialogFragment : DialogFragment() {
         return createContentView(inflater, container, savedInstanceState)
     }
 
+    override fun getTheme(): Int {
+        return R.style.OkTheme_FullScreenDialog
+    }
+
     abstract fun createContentView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // 设置全屏参数
+        dialog?.window?.apply {
+            setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT)) // 透明背景
+            setWindowAnimations(R.style.OkTheme_WindowFadeAnim)
+        }
     }
 
 }
